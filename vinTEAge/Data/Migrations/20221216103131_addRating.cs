2e06aecd9668cd4d904beb159378d3901cabeaac
MirunaGeorgescu <mirubi12@gmail.com>
2022-12-16 13:01:38 +0200

    Adaugare rating in baza de date, creare controller reviews

---
+++ vinTEAge/Data/Migrations/20221216103131_addRating.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vinTEAge.Data.Migrations
+{
+    public partial class addRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Reviews");
+        }
+    }
+}
